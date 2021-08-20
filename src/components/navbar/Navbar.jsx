4470cec20a2cc7b4cb561d4503503158f3f578e2
cpesar <77510555+cpesar@users.noreply.github.com>
2021-08-19 22:50:55 -0600

    working on navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,16 +1,36 @@
 // import React from 'react'
 import './navbar.scss';
+import { GitHub, LinkedIn, Email } from '@material-ui/icons';
 
 export default function Navbar() {
   return (
-    <div className='navbar'>
+    <div className='navbar active'>
       <div className='wrapper'>
         <div className='left'>
-          <a href='#intro'>This is my introduction</a>
+          <a href='#intro' className='logo'>geuius.</a>
+          <div className='itemContainer'>
+            <GitHub className="icon"/>
+            <span>cpesar</span>
+          </div>
+
+          <div className='itemContainer'>
+            <LinkedIn className="icon" />
+            <span>chrispesar1@gmail.com</span>
+          </div>
+
+          <div className='itemContainer'>
+            <Email className="icon" />
+            <span>chrispesar1@gmail.com</span>
+          </div>
+          
          
         </div>
         <div className='right'>
-          <h1>This is right</h1>
+          <div className='hamburger'>
+            <span className='line1'></span>
+            <span className='line2'></span>
+            <span className='line3'></span>
+          </div>
         </div>
       </div>
     </div>
