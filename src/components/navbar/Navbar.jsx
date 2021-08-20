28cfcec8c2dd6af813ffad2195e9cb673ab8e424
cpesar <77510555+cpesar@users.noreply.github.com>
2021-08-19 22:08:34 -0600

    more styling

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,7 +4,15 @@ import './navbar.scss';
 export default function Navbar() {
   return (
     <div className='navbar'>
-      
+      <div className='wrapper'>
+        <div className='left'>
+          <a href='#intro'>This is my introduction</a>
+         
+        </div>
+        <div className='right'>
+          <h1>This is right</h1>
+        </div>
+      </div>
     </div>
   )
 }
