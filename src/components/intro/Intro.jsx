75976d6f608e91ad061de8aa5bec8e0a6d5159f4
cpesar <77510555+cpesar@users.noreply.github.com>
2021-08-21 15:44:16 -0600

    added animation to intro section

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,8 +1,21 @@
 
 import './intro.scss';
 import { KeyboardArrowDown } from '@material-ui/icons';
+import { init } from 'ityped';
+import { useEffect, useRef } from 'react';
 
 export default function Intro() {
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed: 60,
+      strings: ['Web Developer', 'Learner', 'Snowboarder', 'Human'],
+    });
+  },[]);
+
   return (
     <div className='intro' id='intro'>
       <div className='left'>
@@ -15,7 +28,9 @@ export default function Intro() {
         <div className='wrapper'>
           <h2>Hello I'm </h2>
           <h1>Chris Pesar</h1>
-          <h3>Web Developer<span></span></h3>
+          <h3>
+            <span ref={textRef}></span>
+          </h3>
         </div>
         <a href="#portfolio">
           <span className='arrow'>
